7fb80ff4bd43859e2524718d78e97fce2830bb0c
Waranon021 <waranon-021@hotmail.co.th>
2026-08-18 21:03:57 +0700

    chore: configure Django with PostgreSQL

---
+++ config/__init__.py
@@ -0,0 +1,2 @@
+# บอก Python ว่า directory นี้เป็น Python package ตรงนี้เริ่มเชื่อมกับ Python concepts ที่คุณกำลังเรียน:
+# ทำให้ Python สามารถ import: import config หรือ from config import settings
